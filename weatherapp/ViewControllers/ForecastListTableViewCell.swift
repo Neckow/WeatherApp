38cc0c5e88eac3b0e26fc18fb64f5f65110d8237
Loic B <loicb@MacBook-Pro-de-Babal.local>
2018-05-11 17:42:58 +0200

    Display webservice data

---
+++ weatherapp/ViewControllers/ForecastListTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoListTableViewCell.swift
+//  weatherapp
+//
+//  Created by Loic B on 07/05/2018.
+//  Copyright © 2018 Loic B. All rights reserved.
+//
+
+import UIKit
+
+class ForecastListTableViewCell: UITableViewCell { //externalise
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    func configure(with viewModel: ForecastListCellViewModel) {
+        self.titleLabel.text = viewModel.day
+        self.detailLabel.text = viewModel.temp_min!+" "+viewModel.temp_max!
+    }
+}
--- weatherapp/ViewControllers/PhotoListTableViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PhotoListTableViewCell.swift
-//  weatherapp
-//
-//  Created by Loic B on 07/05/2018.
-//  Copyright © 2018 Loic B. All rights reserved.
-//
-
-import Foundation
